cf373052c49781960037e944390ce36736dd75d9
serkanaydin <serkanaydin@marun.edu.tr>
2021-03-07 01:15:52 +0300

    sntab test

--- new.c
+++ new.c
@@ -10,123 +10,132 @@ struct table{
     int value;
 };
 
+struct table sntab[200];
 struct label LABEL[120];
 struct table OPNTAB[54];
 short program[20000];
-/*void initializeSntab(){
+int labelSearch(char* str){
+    int index=0;
+    for(int i=0;i<114;i++){
+        if(strcmp(LABEL[i].name,str)==0){
+            return i;}
+    }
+    return -1;
+}
+void initializeSntab(){
     sntab[0].name="rem";
-    sntab[0].address=labelSearch("srem");
+    sntab[0].value=labelSearch("srem");
     sntab[1].name="data";
-    sntab[1].address=labelSearch("sdata");
+    sntab[1].value=labelSearch("sdata");
     sntab[2].name="input";
-    sntab[2].address=labelSearch("sinput");
+    sntab[2].value=labelSearch("sinput");
     sntab[3].name="color";
-    sntab[3].address=labelSearch("scolor");
+    sntab[3].value=labelSearch("scolor");
     sntab[4].name="list ";
-    sntab[4].address=labelSearch("slist");
+    sntab[4].value=labelSearch("slist");
     sntab[5].name="enter";
-    sntab[5].address=labelSearch("senter");
+    sntab[5].value=labelSearch("senter");
     sntab[6].name="let";
-    sntab[6].address=labelSearch("slet");
+    sntab[6].value=labelSearch("slet");
     sntab[7].name="if";
-    sntab[7].address=labelSearch("sif");
+    sntab[7].value=labelSearch("sif");
     sntab[8].name="for";
-    sntab[8].address=labelSearch("sfor");
+    sntab[8].value=labelSearch("sfor");
     sntab[9].name="next";
-    sntab[9].address=labelSearch("snext");
+    sntab[9].value=labelSearch("snext");
     sntab[10].name="goto";
-    sntab[10].address=labelSearch("sgoto");
+    sntab[10].value=labelSearch("sgoto");
     sntab[11].name="go to";
-    sntab[11].address=labelSearch("sgoto");
+    sntab[11].value=labelSearch("sgoto");
     sntab[12].name="gosub";
-    sntab[12].address=labelSearch("sgosub");
+    sntab[12].value=labelSearch("sgosub");
     sntab[13].name="trap";
-    sntab[13].address=labelSearch("strap");
+    sntab[13].value=labelSearch("strap");
     //  sntab[14].name="bye";                                                            //DOESNT EXIST
-    //  sntab[14].address=labelSearch("sbye");
+    //  sntab[14].value=labelSearch("sbye");
     sntab[15].name="cont";
-    sntab[15].address=labelSearch("scont");
-    sntab[16].name="call";
-    sntab[16].address=labelSearch("scall");
+    sntab[15].value=labelSearch("scont");
+    sntab[16].name="com";
+    sntab[16].value=labelSearch("scom");
     sntab[17].name="close";
-    sntab[17].address=labelSearch("sclose");
+    sntab[17].value=labelSearch("sclose");
     sntab[18].name="clr";
-    sntab[18].address=labelSearch("sclr");
+    sntab[18].value=labelSearch("sclr");
     sntab[19].name="deg";
-    sntab[19].address=labelSearch("sdeg");
+    sntab[19].value=labelSearch("sdeg");
     sntab[20].name="dim";
-    sntab[20].address=labelSearch("sdim");
+    sntab[20].value=labelSearch("sdim");
     sntab[21].name="end";
-    sntab[21].address=labelSearch("send");
+    sntab[21].value=labelSearch("send");
     sntab[22].name="new";
-    sntab[22].address=labelSearch("snew");
+    sntab[22].value=labelSearch("snew");
     sntab[23].name="open";
-    sntab[23].address=labelSearch("sopen");
+    sntab[23].value=labelSearch("sopen");
     sntab[24].name="load";
-    sntab[24].address=labelSearch("sload");
+    sntab[24].value=labelSearch("sload");
     sntab[25].name="save";
-    sntab[25].address=labelSearch("ssave");
+    sntab[25].value=labelSearch("ssave");
     sntab[26].name="status";
-    sntab[26].address=labelSearch("sstatus");
+    sntab[26].value=labelSearch("sstatus");
     sntab[27].name="note";
-    sntab[27].address=labelSearch("snote");
+    sntab[27].value=labelSearch("snote");
     sntab[28].name="point";
-    sntab[28].address=labelSearch("spoint");
+    sntab[28].value=labelSearch("spoint");
     sntab[29].name="xio";
-    sntab[29].address=labelSearch("sxio");
+    sntab[29].value=labelSearch("sxio");
     sntab[30].name="on";
-    sntab[30].address=labelSearch("son");
+    sntab[30].value=labelSearch("son");
     sntab[31].name="poke";
-    sntab[31].address=labelSearch("spoke");
+    sntab[31].value=labelSearch("spoke");
     sntab[32].name="print";
-    sntab[32].address=labelSearch("sprint");
+    sntab[32].value=labelSearch("sprint");
     sntab[33].name="rad";
-    sntab[33].address=labelSearch("srad");
+    sntab[33].value=labelSearch("srad");
     sntab[34].name="read";
-    sntab[34].address=labelSearch("sread");
+    sntab[34].value=labelSearch("sread");
     sntab[35].name="restore";
-    sntab[35].address=labelSearch("srest");
+    sntab[35].value=labelSearch("srest");
     sntab[36].name="return";
-    sntab[36].address=labelSearch("sret");
+    sntab[36].value=labelSearch("sret");
     sntab[37].name="run";
-    sntab[37].address=labelSearch("srun");
+    sntab[37].value=labelSearch("srun");
     sntab[38].name="stop";
-    sntab[38].address=labelSearch("sstop");
+    sntab[38].value=labelSearch("sstop");
     sntab[39].name="pop";
-    sntab[39].address=labelSearch("spop");
+    sntab[39].value=labelSearch("spop");
     sntab[40].name="?";
-    sntab[40].address=labelSearch("sprint");
+    sntab[40].value=labelSearch("sprint");
     sntab[41].name="get";
-    sntab[41].address=labelSearch("sget");
+    sntab[41].value=labelSearch("sget");
     sntab[42].name="put";
-    sntab[42].address=labelSearch("sput");
+    sntab[42].value=labelSearch("sput");
     sntab[43].name="graphics";
-    sntab[43].address=labelSearch("sgr");
+    sntab[43].value=labelSearch("sgr");
     sntab[44].name="plot";
-    sntab[44].address=labelSearch("splot");
+    sntab[44].value=labelSearch("splot");
     sntab[45].name="position";
-    sntab[45].address=labelSearch("spos");
+    sntab[45].value=labelSearch("spos");
     sntab[46].name="dos";
-    sntab[46].address=labelSearch("sdos");
+    sntab[46].value=labelSearch("sdos");
     sntab[47].name="drawto";
-    sntab[47].address=labelSearch("sdrawto");                            //*SDRAWTO
+    sntab[47].value=labelSearch("sdrawto");                            //*SDRAWTO
     sntab[48].name="setcolor";
-    sntab[48].address=labelSearch("ssetcolor");
+    sntab[48].value=labelSearch("ssetcolor");
     sntab[49].name="locate";
-    sntab[49].address=labelSearch("slocate");                            //*slocate
+    sntab[49].value=labelSearch("slocate");                            //*slocate
     sntab[50].name="sound";
-    sntab[50].address=labelSearch("ssound");
+    sntab[50].value=labelSearch("ssound");
     sntab[51].name="lprint";
-    sntab[51].address=labelSearch("slprint");
+    sntab[51].value=labelSearch("slprint");
     sntab[52].name="csave";
-    sntab[52].address=labelSearch("scsave");
+    sntab[52].value=labelSearch("scsave");
     sntab[53].name="cload";
-    sntab[53].address=labelSearch("scload");
+    sntab[53].value=labelSearch("scload");
     sntab[54].name="sound";
-    sntab[54].address=labelSearch("ssound");
+    sntab[54].value=labelSearch("ssound");
     //??? SILET
 
-}*/
+}
 
 int opnTabSearch(char* str){
     int index=0;
@@ -138,14 +147,7 @@ int opnTabSearch(char* str){
     return -1;
 }
 
-int labelSearch(char* str){
-    int index=0;
-    for(int i=0;i<114;i++){
-        if(strcmp(LABEL[i].name,str)==0){
-            return i;}
-    }
-    return -1;
-}
+
 
 int main(void){
     FILE *fp;
@@ -261,14 +263,14 @@ int main(void){
 
 
 
-    for(int k=0;k<110;k++){
-        printf("label %s, address:%d\n",LABEL[k].name,LABEL[k].location);
+ /*   for(int k=0;k<110;k++){
+        printf("label %s, value:%d\n",LABEL[k].name,LABEL[k].location);
     }
     for(int k=0;k<54;k++){
         printf("operator %s, value:%x\n",OPNTAB[k].name,OPNTAB[k].value);
     }
-    for(int k=0; k<20000;k++){
+    for(int k=0; k<1000;k++){
         printf("program %d value:%x\n",k,program[k]);
-    }
-    //initializeSntab();
+    }*/
+    initializeSntab();
 }
